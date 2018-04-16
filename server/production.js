81cbd7e90cbe98854db16e3d022c90be16fcbbf5
Alexander Almström <info@alexanderalmstrom.com>
2018-04-17 00:27:00 +0200

    Cleaned up prod server

--- server/production.js
+++ server/production.js
@@ -6,13 +6,10 @@ const morgan = require('morgan')
 const app = express()
 const server = http.createServer(app)
 
-// Setup logger
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'))
 
-// Serve static assets
 app.use(express.static(path.resolve(__dirname, '..', 'build')))
 
-// Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'))
 })
